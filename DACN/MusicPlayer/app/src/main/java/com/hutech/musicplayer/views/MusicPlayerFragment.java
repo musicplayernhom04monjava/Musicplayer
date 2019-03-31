1b9415185c17f804c0461109ec68b5739fbd972b
Hoài Nguyễn <panpansiku@gmail.com>
2019-03-31 14:42:57 +0700

    no message

--- DACN/MusicPlayer/app/src/main/java/com/hutech/musicplayer/views/MusicPlayerFragment.java
+++ DACN/MusicPlayer/app/src/main/java/com/hutech/musicplayer/views/MusicPlayerFragment.java
@@ -1,4 +1,4 @@
-package com.hutech.musicplayer.views;
+﻿package com.hutech.musicplayer.views;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -76,47 +76,7 @@ public class MusicPlayerFragment extends Fragment implements IMusicPlayer{
         searchSongs();
     }
     /*
-    public void addSong()
-    {
-        songs = new ArrayList<>();
-        //songs.add(new Song("My Love","Westlife",R.raw.my_love_lrc,R.raw.my_love_mp3));
-        //songs.add(new Song("Only Love","Westlife",R.raw.only_love_lrc,R.raw.only_love_mp3));
-        musicPlayerAdapter = new MusicPlayerAdapter(getActivity(),songs);
-        recyclerView.setAdapter(musicPlayerAdapter);
-    }*/
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onEvent(Integer currentPosition)
-    {
-        //Log.d("MusicPlayerFragment","position"+position);
-        //Log.d("MusicPlayerFrag","position"+customEventBus.getPosition());
-        Toast.makeText(view.getContext(),"Vị trí:"+currentPosition,Toast.LENGTH_LONG).show();
-        musicPlayerAdapter.updateItem(currentPosition);
-
-    }
-
-    @Override
-    public void listSong(List<Song> songs) {
-        this.songs = new ArrayList<>();
-        this.songs = songs;
-        musicPlayerAdapter = new MusicPlayerAdapter(getActivity(),songs);
-        recyclerView.setAdapter(musicPlayerAdapter);
-        Log.d("MusicPlayerFragment","listSong");
-    }
-
-    @Override
-    public void onFailure(ErrorCode errorCode) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext())
-                .setTitle("Thông báo")
-                .setMessage(errorCode.message())
-                .setNegativeButton("Đồng ý", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        builder.create().show();
-    }
+    
 
     
 
