3d013f6da1a39b47eced65aa6de381ac1755327e
DESKTOP-MUK6QIV\HAO <psinhhao96@gmail.com>
2019-03-31 12:46:46 +0700

    Revert "Upload source main"
    This reverts commit 9106a70f7e978a664c9c2ba115804e8f594b2dff.

--- DACN/MusicPlayer/app/src/main/java/com/hutech/musicplayer/managers/IValidate.java
+++
@@ -1,5 +0,0 @@
-package com.hutech.musicplayer.managers;
-
-public interface IValidate {
-    boolean validate() throws Exception;
-}
--- DACN/MusicPlayer/app/src/main/java/com/hutech/musicplayer/views/ISignUpActivity.java
+++
@@ -1,8 +0,0 @@
-package com.hutech.musicplayer.views;
-
-import com.hutech.musicplayer.managers.ErrorCode;
-
-public interface ISignUpActivity {
-    void onSuccess(String message);
-    void onFailure(ErrorCode errorCode);
-}
